1931e90cfe58830270eebf8ab96ee7ae0d6f7f35
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-12-31 17:43:33 +0100

    initial files setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+   <div className="container">
+     <div className="row">
+       <div className="col-6">col 1</div>
+       <div className="col-6"><span><i className="fas fa-home"></i></span></div>
+     </div>
+   </div>
+  );
+}
+
+export default App;
