cffcdabdae5f9054bc179a082af816f8f2c0e1bb
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-01-13 00:20:11 +0100

    navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom'
+import logo from '../logo.svg'
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-             <h4>Nav says Hii</h4>
-            </div>
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark px-sm-5">
+        <Link to="/">
+        <img src={logo} alt="logo" className="navbar-brand"/>
+        </Link>
+        <ul className="navbar-bar align-item-center">
+        <li className="nav-item ml-5">
+        <Link to="/products" className="nav-link">
+        Products
+        </Link>
+        </li>
+        </ul>
+        <Link to="/cart" className="nav-link ml-auto">
+        <button>
+        <i className="fas fa-cart-plus"/>
+        my cart
+        </button>
+        </Link>
+        </nav>
         )
-    }
+   }
 }
