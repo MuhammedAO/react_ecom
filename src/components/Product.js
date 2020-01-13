602e44f1639b9211906c6aa97e85175d6774bd57
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-01-13 13:04:36 +0100

    Products page display

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,10 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
 export default class Product extends Component {
-    state={
-    products:[]
-    }
+   
     render() {
         return (
-            <Fragment>
-            <div className="py-5">
-            <div className="container">
-                
-            </div>
-            </div>
-            </Fragment>
+         <div>Hey</div>
         )
     }
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Product from './Product'
+import Title from './Title'
 
 
 export default class ProductList extends Component {
+    state={
+        products:[]
+        }
     render() {
         return (
-            <div>
-               <Product/>
-            </div>
+        <Fragment>   
+        <div className="py-5">
+        <div className="container">
+        <Title name="Our" title="products"/>
+        <div className="row">
+        </div>      
+        </div>
+        </div>
+        </Fragment>
         )
     }
 }
