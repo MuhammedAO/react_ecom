826fbbb905f10f7c1767e12d3694b29a2e820a26
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-01-22 22:39:02 +0100

    styling not-found page

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react'
 export default class Default extends Component {
     render() {
         return (
-            <div>
-                <h4>Not Found</h4>
+            <div className="container">
+            <div className="row">
+            <div className="col-10 mx-auto text-center text-capitalize pt-5">
+            <h1 className="display-3">404</h1>
+            <h1>error</h1>
+            <h2>page not found</h2>
+            <h3>the requested url <span className="text-danger">{this.props.location.pathname}</span>{" "} was not found</h3>
+            </div> 
+            </div>
             </div>
         )
     }
